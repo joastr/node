d7020f2c3cb474b4a1095c5bc14ebdb5bc589e60
Joanna Kasperek <joastr1@gmail.com>
2018-12-10 12:09:37 +0100

    initial commit

---
+++ program.js
@@ -0,0 +1,44 @@
+process.stdin.setEncoding('utf-8');
+/*
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/exit') {
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        } else {
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
+
+*/
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    //var instruction = input.toString().trim();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+    
+        switch(instruction) {
+            case  '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+            case '/process.env':
+            process.stdout.write('node version\n');
+            process.stdout.write(process.version);
+            process.stdout.write('\n\nOS version\n');
+            process.stdout.write(process.env.OS + '\n');
+            break;
+            default:
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
+
+
+
